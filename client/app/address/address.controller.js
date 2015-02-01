b2af4884bda1cce7a55adb694eec0255f9964e16
Kazuki Kanamori <yogurito@gmail.com>
2015-02-01 17:43:32 +0900

    window.alert should be $window.alert

--- client/app/address/address.controller.js
+++ client/app/address/address.controller.js
@@ -1,16 +1,15 @@
 'use strict';
 
 angular.module('AngularJsTestson')
-  .controller('AddressCtrl', function ($scope, $state, cartItem, Purchase) {
+  .controller('AddressCtrl', function ($scope, $state, cartItem, Purchase, $window) {
     $scope.isLoading = false;
     $scope.order = Purchase;
 
-    console.log($scope.order.purchases);
     $scope.payment = function(){
       $scope.isLoading=true;
       $scope.order.purchases = cartItem.getCartItems();
       Purchase.order(function(data){
-        window.alert(data.message);
+        $window.alert(data.message);
         cartItem.clear();
         $state.go('main.app.home');
       });
