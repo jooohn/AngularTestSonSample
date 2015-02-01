8ac30eef37e311fd3fc013ced0390946b56065d4
Kazuki Kanamori <yogurito@gmail.com>
2015-02-01 17:32:21 +0900

    add AddressCtrl spec

--- client/app/address/address.controller.js
+++ client/app/address/address.controller.js
@@ -1,20 +1,15 @@
 'use strict';
 
 angular.module('AngularJsTestson')
-  .controller('AddressCtrl', function ($scope, $state, cartItem, Purchase, $log) {
+  .controller('AddressCtrl', function ($scope, $state, cartItem, Purchase) {
     $scope.isLoading = false;
+    $scope.order = Purchase;
+
+    console.log($scope.order.purchases);
     $scope.payment = function(){
       $scope.isLoading=true;
-      var p = new Purchase();
-      p.firstName = $scope.order.firstName;
-      p.lastName = $scope.order.lastName;
-      p.tel = $scope.order.tel;
-      p.address = $scope.order.address;
-      p.mailaddress = $scope.order.mailaddress;
-      p.purchases = cartItem.getCartItems();
-
-      $log.info(p);
-      p.$save(function(data){
+      $scope.order.purchases = cartItem.getCartItems();
+      Purchase.order(function(data){
         window.alert(data.message);
         cartItem.clear();
         $state.go('main.app.home');
--- client/app/address/address.controller.spec.js
+++ client/app/address/address.controller.spec.js
@@ -5,17 +5,43 @@ describe('Controller: AddressCtrl', function () {
   // load the controller's module
   beforeEach(module('AngularJsTestson'));
 
-  var AddressCtrl, scope;
+  var AddressCtrl, scope, $httpBackend, _cartItem;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, cartItem) {
     scope = $rootScope.$new();
+    _cartItem = cartItem;
+    _cartItem.add({
+      price: 2000,
+      productId: 10,
+      productName: 'productName'
+    });
     AddressCtrl = $controller('AddressCtrl', {
       $scope: scope
     });
   }));
+  beforeEach(inject(function($injector, $templateCache){
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.whenGET('components/views/navbar/navbar.html').respond(200, '');
+    $httpBackend.whenGET('components/views/footer/footer.html').respond(200, '');
+    $httpBackend.whenGET('app/home/home.html').respond(200, '');
+    $templateCache.put('app/frame/main.html', 'components/views/navbar/navbar.html', 'components/views/footer/footer.html');
+  }));
+  beforeEach(inject(function($state){
+    spyOn($state, 'go');
+  }));
+  beforeEach(inject(function($window){
+    spyOn($window, 'alert');
+  }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
-  });
+  it('should set isLoading to be true after $scope.payment called', inject(function ($state, $window) {
+    $httpBackend.expectPOST('/api/purchases').respond({'status':'ok','message':'transaction completed'});
+    expect(scope.isLoading).toBe(false);
+    scope.payment();
+    expect(scope.isLoading).toBe(true);
+    $httpBackend.flush();
+    expect($window.alert).toHaveBeenCalledWith('transaction completed');
+    expect($state.go).toHaveBeenCalledWith('main.app.home');
+    expect(_cartItem.getCartItems().length).toBe(0);
+  }));
 });
