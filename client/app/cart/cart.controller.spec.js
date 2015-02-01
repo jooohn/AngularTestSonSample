319912c4dda1fde11b6854b6efa0d55630d8fbe4
jooohn <tomioka@talentio.com>
2015-02-01 15:45:26 +0900

    added test for getCartItems call to CartCtrl spec

--- client/app/cart/cart.controller.spec.js
+++ client/app/cart/cart.controller.spec.js
@@ -5,29 +5,36 @@ describe('Controller: CartCtrl', function () {
   // load the controller's module
   beforeEach(module('AngularJsTestson'));
 
-  var CartCtrl, scope, CartItem, testProduct1;
-  testProduct1 = {
-    productId: 1
-  };
+  var CartCtrl, scope, CartItem, testProduct1, testProduct2;
+  testProduct1 = { productId: 1 };
+  testProduct2 = { productId: 2 };
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, cartItem) {
     scope = $rootScope.$new();
     CartItem = cartItem;
-    CartCtrl = $controller('CartCtrl', {
-      $scope: scope
-    });
-
+    spyOn(CartItem, 'getCartItems')
+      .andReturn(['dummy1', 'dummy2']);
     spyOn(CartItem, 'add');
     spyOn(CartItem, 'remove');
     spyOn(CartItem, 'clear');
+
+    CartCtrl = $controller('CartCtrl', {
+      $scope: scope
+    });
   }));
 
-  it('should cart items empty', function() {
-    expect(scope.cartItems.length).toEqual(0);
+  it('should spied method called expected times', function() {
+    expect(CartItem.getCartItems).toHaveBeenCalled();
+    expect(CartItem.getCartItems.calls.length).toEqual(1);
+
     expect(CartItem.add).not.toHaveBeenCalled();
     expect(CartItem.remove).not.toHaveBeenCalled();
     expect(CartItem.clear).not.toHaveBeenCalled();
+  });
+
+  it('should cartItems set 2 items', function() {
+    expect(scope.cartItems.length).toEqual(2);
   });
 
   it('should call cartItem.add', function() {
