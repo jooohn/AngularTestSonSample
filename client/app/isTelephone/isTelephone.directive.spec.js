dac2c63f0e9edb1c75c7965d2a7ef2da0fbb0579
Kazuki Kanamori <yogurito@gmail.com>
2015-02-01 14:18:22 +0900

    write unit test for isTelephone direct

--- client/app/isTelephone/isTelephone.directive.spec.js
+++ client/app/isTelephone/isTelephone.directive.spec.js
@@ -5,17 +5,41 @@ describe('Directive: isTelephone', function () {
   // load the directive's module
   beforeEach(module('AngularJsTestson'));
 
-  var element,
-    scope;
+  var element, scope, form;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile) {
     scope = $rootScope.$new();
-  }));
-
-  it('can be instantiated via attribute', inject(function($compile){
-    element = angular.element('<input type="tel" is-telephone ng-model="order.tel">');
+    element = angular.element('<form name="form"><input type="tel" name="telephone" is-telephone ng-model="model.telephone"></form>');
+    scope.model = {telephone: null};
     element = $compile(element)(scope);
-    expect(element).not.toBeNull();
+    form = scope.form;
   }));
 
+  it('accepts digits only', function(){
+    form.telephone.$setViewValue('0123456789');
+    scope.$digest();
+    expect(form.telephone.$valid).toBe(true);
+    expect(scope.model.telephone).toEqual('0123456789');
+  });
+
+  it('rejects blank string', function(){
+    form.telephone.$setViewValue('');
+    scope.$digest();
+    expect(form.telephone.$invalid).toBe(true);
+  });
+
+  it('rejects string with space', function(){
+    form.telephone.$setViewValue(' ');
+    scope.$digest();
+    expect(form.telephone.$invalid).toBe(true);
+  });
+
+  it('rejects string with non-digits', function(){
+    form.telephone.$setViewValue('03-1234-5678');
+    scope.$digest();
+    expect(form.telephone.$invalid).toBe(true);
+    form.telephone.$setViewValue('+811234567890');
+    scope.$digest();
+    expect(form.telephone.$invalid).toBe(true);
+  });
 });
