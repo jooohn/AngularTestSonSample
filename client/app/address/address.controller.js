db93eb592293b00846661f4857d1a3fd39327840
Kazuki Kanamori <yogurito@gmail.com>
2015-02-01 14:41:59 +0900

    modify AddressCtrl to get items with getCartItems()

--- client/app/address/address.controller.js
+++ client/app/address/address.controller.js
@@ -5,22 +5,13 @@ angular.module('AngularJsTestson')
     $scope.isLoading = false;
     $scope.payment = function(){
       $scope.isLoading=true;
-      $log.info(cartItem.items);
-      var items=[];
-      angular.forEach(cartItem.items, function(v){
-        items.push({
-          productId: v.productId,
-          quantity: 1
-        });
-      });
-
       var p = new Purchase();
       p.firstName = $scope.order.firstName;
       p.lastName = $scope.order.lastName;
       p.tel = $scope.order.tel;
       p.address = $scope.order.address;
       p.mailaddress = $scope.order.mailaddress;
-      p.purchases = items;
+      p.purchases = cartItem.getCartItems();
 
       $log.info(p);
       p.$save(function(data){
