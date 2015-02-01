6d12278f894f9eac096d04fb793c711ff5b52b2f
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-02-01 14:17:49 +0900

    Add some specs to cartItem :beers:

--- client/app/cartItem/cartItem.service.js
+++ client/app/cartItem/cartItem.service.js
@@ -5,30 +5,66 @@ angular.module('AngularJsTestson')
 
     var items = [];
 
+    /////////////////////
+    // private API here
+
+    function indexOfCartItem(productId) {
+      var result = -1;
+      angular.forEach(items, function(item, index){
+        if(this.keepGoing) {
+          if(item.productId === productId) {
+            result = index;
+          }
+        }
+      }, {
+        keepGoing: true
+      });
+      return result;
+    }
+
+    function increment(index) {
+
+      items[index].count++;
+      items[index].totalAmount += items[index].price;
+      return items;
+
+    }
+
+    function append(product){
+
+      var newProduct = {
+        productId: product.productId,
+        productName: product.productName,
+        price: product.price,
+        count: 1,
+        totalAmount: product.price,
+        product: product
+      };
+      items.push(newProduct);
+      return items;
+
+    }
+
+    ///////////////////// 
     // Public API here
     return {
-      add: function (item) {
-        return items.push(item);
+      add: function (product) {
+        var index = indexOfCartItem(product.productId);
+        return (index > -1) ? increment(index): append(product);
       },
-      remove: function(item) {
-        var index = items.indexOf(item);
+      remove: function(productId) {
+        var index = indexOfCartItem(productId);
         if(index > -1) {
           items.splice(index, 1);
         }
       },
-      removeProductId: function(productId) {
-        angular.forEach(items, function(v,k){
-          if(this.keepGoing) {
-            if (v.productId === parseInt(productId)){
-              items.splice(k, 1);
-              this.keepGoing = false;
-            }
-          }
-        },{keepGoing:true});
-      },
       clear: function() {
         items.length = 0;
       },
-      items: items
+      getCartItems: function(){
+        // [MEMO] 参照渡しなので取り扱い注意。値渡しにする場合は、cloneとかしてね
+        return items;
+      }
     };
+    
   });
--- client/app/cartItem/cartItem.service.spec.js
+++ client/app/cartItem/cartItem.service.spec.js
@@ -6,13 +6,111 @@ describe('Service: cartItem', function () {
   beforeEach(module('AngularJsTestson'));
 
   // instantiate service
-  var cartItem;
+  var cartItem, 
+    product = [];
+
   beforeEach(inject(function (_cartItem_) {
     cartItem = _cartItem_;
+    cartItem.clear();
+
+    // dummy data
+    product.push({
+      price: 2000,
+      productId: 10,
+      productName: 'はじめてのAngularJS(和書)'
+    });
+    product.push({
+      price: 1000,
+      productId: 11,
+      productName: 'AngularJSリファレンス'
+    });
+
   }));
 
-  it('should do something', function () {
-    expect(!!cartItem).toBe(true);
+  describe('add()のテスト', function(){
+
+    beforeEach(function(){});
+
+    it('新規で1件追加できること', function(){
+
+      expect(cartItem.getCartItems().length).toBe(0);
+      cartItem.add(product[0]);
+      expect(cartItem.getCartItems().length).toBe(1);
+      expect(cartItem.getCartItems()[0]).toEqual({
+        productId: 10,
+        productName: 'はじめてのAngularJS(和書)',
+        price: 2000,
+        count: 1,
+        totalAmount: 2000,
+        product: product[0]
+      });
+
+    });
+
+    it('既にカート内に存在する場合はインクリメントされること', function(){
+
+      cartItem.add(product[0]);
+      expect(cartItem.getCartItems().length).toBe(1);
+
+      cartItem.add(product[0]);
+      expect(cartItem.getCartItems().length).toBe(1);
+      expect(cartItem.getCartItems()[0]).toEqual({
+        productId: 10,
+        productName: 'はじめてのAngularJS(和書)',
+        price: 2000,
+        count: 2,
+        totalAmount: 4000,
+        product: product[0]
+      });
+
+    });
+
+  });
+
+  describe('remove()のテスト', function(){
+
+    beforeEach(function(){
+      cartItem.add(product[0]);
+      cartItem.add(product[1]);
+    });
+
+    it('1件削除できること', function(){
+      expect(cartItem.getCartItems().length).toBe(2);
+      cartItem.remove(product[1].productId);
+      expect(cartItem.getCartItems().length).toBe(1);
+      expect(cartItem.getCartItems()[0].productId).toBe(10);
+    });
+
+  });
+
+  describe('clear()のテスト', function(){
+
+    beforeEach(function(){
+      cartItem.add(product[0]);
+      cartItem.add(product[1]);
+    });
+
+    it('カートがクリアされること', function(){
+      expect(cartItem.getCartItems().length).toBe(2);
+      cartItem.clear();
+      expect(cartItem.getCartItems().length).toBe(0);
+    });
+
+  });
+
+  describe('getCartItems()のテスト', function(){
+
+    beforeEach(function(){
+      cartItem.add(product[0]);
+      cartItem.add(product[1]);
+    });
+
+    it('itemsが取得できること', function(){
+      expect(cartItem.getCartItems().length).toBe(2);
+      expect(cartItem.getCartItems()[0].productId).toBe(10);
+      expect(cartItem.getCartItems()[1].productId).toBe(11);
+    });
+
   });
 
 });
